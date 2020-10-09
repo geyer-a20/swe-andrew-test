3e3979ac4272d94914d4e6a2e04ca00b7b2d9df3
geyer-a20 <60201528+geyer-a20@users.noreply.github.com>
2020-10-09 15:30:09 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :password_resets
-  resources :log
+  #resources :log
+  match '/log/index' => 'log#index'
   match '/log/:format' => 'log#index'
   root 'sessions#new'
 
